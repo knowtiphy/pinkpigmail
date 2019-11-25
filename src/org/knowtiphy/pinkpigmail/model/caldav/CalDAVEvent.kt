de0f5efa474581176fb0e52bf85d7eb5f9988c39
aardy1 <graham@orangedogsoftware.com>
2019-11-25 14:33:17 -0800

    fudge for dates

--- src/org/knowtiphy/pinkpigmail/model/caldav/CalDAVEvent.kt
+++ src/org/knowtiphy/pinkpigmail/model/caldav/CalDAVEvent.kt
@@ -6,10 +6,14 @@ import org.knowtiphy.babbage.storage.IStorage
 import org.knowtiphy.babbage.storage.Vocabulary
 import org.knowtiphy.pinkpigmail.model.PPPeer
 import org.knowtiphy.utils.JenaUtils
+import java.time.LocalDateTime
 
 class CalDAVEvent(accountId: String, storage: IStorage) : PPPeer(accountId, storage)
 {
     val event = Entry<String>("")
+
+    var startDate: LocalDateTime? = null
+    var endDate: LocalDateTime? = null
 
     init
     {
@@ -18,23 +22,37 @@ class CalDAVEvent(accountId: String, storage: IStorage) : PPPeer(accountId, stor
         declareU(Vocabulary.HAS_DATE_END, ::setEndDate)
     }
 
+    private fun update()
+    {
+        if (startDate != null && endDate != null)
+        {
+            println("SET EVENT TIME " + id + " :: " + startDate + "::" +endDate)
+            event.changeStartDate(startDate!!.toLocalDate())
+            event.changeStartTime(startDate!!.toLocalTime())
+            event.changeEndDate(endDate!!.toLocalDate())
+            event.changeEndTime(endDate!!.toLocalTime())
+        }
+    }
+
     private fun setStartDate(stmt: Statement)
     {
-        val dt = JenaUtils.getLDT(stmt.literal)
-        println("START DATE = "+ dt)
-        println("START DATE = "+ dt.toLocalDate())
-        println("START DATE = "+ dt.toLocalTime())
-        event.changeStartDate(dt.toLocalDate())
-        event.changeStartTime(dt.toLocalTime())
+        startDate = JenaUtils.getLDT(stmt.literal)
+        update()
+//        println("START DATE = "+ dt)
+//        println("START DATE = "+ dt.toLocalDate())
+//        println("START DATE = "+ dt.toLocalTime())
+//        event.changeStartDate(dt.toLocalDate())
+//        event.changeStartTime(dt.toLocalTime())
     }
 
     private fun setEndDate(stmt: Statement)
     {
-        val dt = JenaUtils.getLDT(stmt.literal)
-        println("END DATE = "+ dt)
-        println("END DATE = "+ dt.toLocalDate())
-        println("END TIME = "+ dt.toLocalTime())
-event.changeEndDate(dt.toLocalDate())
-        event.changeEndTime(dt.toLocalTime())
+        endDate = JenaUtils.getLDT(stmt.literal)
+        update()
+//        println("END DATE = "+ dt)
+//        println("END DATE = "+ dt.toLocalDate())
+//        println("END TIME = "+ dt.toLocalTime())
+//event.changeEndDate(dt.toLocalDate())
+//        event.changeEndTime(dt.toLocalTime())
     }
 }
