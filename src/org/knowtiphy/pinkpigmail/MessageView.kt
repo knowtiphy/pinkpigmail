80eb106b1acbc21a4fdb2b709b64aeb2ffbb1401
aardy1 <graham@orangedogsoftware.com>
2019-12-16 19:01:02 -0800

    check for them clicking on a different message after start the process of displaying a first message

--- src/org/knowtiphy/pinkpigmail/MessageView.kt
+++ src/org/knowtiphy/pinkpigmail/MessageView.kt
@@ -41,225 +41,233 @@ import java.util.logging.Logger
  */
 class MessageView(private val service: ExecutorService) : Replacer()
 {
-    private val logger = Logger.getLogger(MessageView::class.qualifiedName)
+	private val logger = Logger.getLogger(MessageView::class.qualifiedName)
 
-    private val messageProperty = SimpleObjectProperty<Pair<IMessage?, Collection<IMessage>>>()
+	private val messageProperty = SimpleObjectProperty<Pair<IMessage?, Collection<IMessage>>>()
 
-    private val viewer = MailViewer()
-    private val noMessageSelected = UIUtils.boxIt(Label(Strings.NO_MESSAGE_SELECTED))
-    private val loading = UIUtils.boxIt(WaitSpinner(Strings.LOADING_MESSAGE))
+	private val viewer = MailViewer()
+	private val noMessageSelected = UIUtils.boxIt(Label(Strings.NO_MESSAGE_SELECTED))
+	private val loading = UIUtils.boxIt(WaitSpinner(Strings.LOADING_MESSAGE))
 
-    private val fromText = Label()
-    private val subjectText = Label()
-    private val toText = Label()
-    private val headerLeft = GridPane()
-    private val receivedOn = Label()
+	private val fromText = Label()
+	private val subjectText = Label()
+	private val toText = Label()
+	private val headerLeft = GridPane()
+	private val receivedOn = Label()
 
-    private val from = Label(Strings.FROM)
-    private val to = Label(Strings.TO)
-    private val subject = Label(Strings.SUBJECT)
+	private val from = Label(Strings.FROM)
+	private val to = Label(Strings.TO)
+	private val subject = Label(Strings.SUBJECT)
 
-    private val loadRemoteAction = ActionHelper.create(Icons.loadRemote(),
-            {
-                (messageProperty.get().first ?: return@create).loadRemoteProperty.set(true)
-            }, Strings.LOAD_REMOTE_CONTENT)
+	private val loadRemoteAction = ActionHelper.create(Icons.loadRemote(),
+			{
+				(messageProperty.get().first ?: return@create).loadRemoteProperty.set(true)
+			}, Strings.LOAD_REMOTE_CONTENT)
 
-    private val trustSenderAction = ActionHelper.create(Icons.trustSender(),
-            {
-                val message = messageProperty.get().first ?: return@create
-                message.mailAccount.trustSender(message.from)
-            }, Strings.TRUST_SENDER)
+	private val trustSenderAction = ActionHelper.create(Icons.trustSender(),
+			{
+				val message = messageProperty.get().first ?: return@create
+				message.mailAccount.trustSender(message.from)
+			}, Strings.TRUST_SENDER)
 
-    private val loadRemote = ButtonHelper.button(loadRemoteAction)
-    private val trustSender = ButtonHelper.button(trustSenderAction)
-    private val trustContentMenu = SplitMenuButton()
-    private val attachmentsMenu = MenuButton()
+	private val loadRemote = ButtonHelper.button(loadRemoteAction)
+	private val trustSender = ButtonHelper.button(trustSenderAction)
+	private val trustContentMenu = SplitMenuButton()
+	private val attachmentsMenu = MenuButton()
 
-    private val buttons = HBox(attachmentsMenu, trustContentMenu, loadRemote, trustSender)
-    private val headerRight = VBox(buttons, receivedOn)
-    private val header = HBox(headerLeft, headerRight)
-    private val messageSpace = VBox(header, viewer)
+	private val buttons = HBox(attachmentsMenu, trustContentMenu, loadRemote, trustSender)
+	private val headerRight = VBox(buttons, receivedOn)
+	private val header = HBox(headerLeft, headerRight)
+	private val messageSpace = VBox(header, viewer)
 
-    private val listener = { _: ObservableValue<out Document?>, _: Document?, document: Document? ->
-        assert(messageProperty.get() != null)
-        if (document != null)
-        {
-            val message = messageProperty.get()!!
-            val account = message.first!!.mailAccount
-            trustContentMenu.items.clear()
-            val externalRefs = HTMLUtils.computeExternalReferences(document)
-            for (ref in externalRefs)
-            {
-                val checkMenuItem = CheckMenuItem(ref)
-                checkMenuItem.isSelected = account.isTrustedProvider(ref)
-                checkMenuItem.selectedProperty().addListener { _, _, newValue ->
-                    (if (newValue) IEmailAccount::trustProvider else IEmailAccount::unTrustProvider)(account, ref)
-                }
-                trustContentMenu.items.add(checkMenuItem)
-            }
+	private val listener = { _: ObservableValue<out Document?>, _: Document?, document: Document? ->
+		assert(messageProperty.get() != null)
+		if (document != null)
+		{
+			val message = messageProperty.get()!!
+			val account = message.first!!.mailAccount
+			trustContentMenu.items.clear()
+			val externalRefs = HTMLUtils.computeExternalReferences(document)
+			for (ref in externalRefs)
+			{
+				val checkMenuItem = CheckMenuItem(ref)
+				checkMenuItem.isSelected = account.isTrustedProvider(ref)
+				checkMenuItem.selectedProperty().addListener { _, _, newValue ->
+					(if (newValue) IEmailAccount::trustProvider else IEmailAccount::unTrustProvider)(account, ref)
+				}
+				trustContentMenu.items.add(checkMenuItem)
+			}
 
-            trustContentMenu.isDisable = externalRefs.isEmpty()
-            loadRemoteAction.disabledProperty().bind(Bindings.or(message.first!!.loadRemoteProperty,
-                    SimpleBooleanProperty(!message.first!!.isHTML || externalRefs.isEmpty())).or(
-                    Bindings.createBooleanBinding(
-                            UIUtils.callable { account.isTrustedSender(message.first!!.from) }, account.trustedSenders)))
-        }
-    }
+			trustContentMenu.isDisable = externalRefs.isEmpty()
+			loadRemoteAction.disabledProperty().bind(Bindings.or(message.first!!.loadRemoteProperty,
+					SimpleBooleanProperty(!message.first!!.isHTML || externalRefs.isEmpty())).or(
+					Bindings.createBooleanBinding(
+							UIUtils.callable { account.isTrustedSender(message.first!!.from) }, account.trustedSenders)))
+		}
+	}
 
-    init
-    {
-        children.addAll(messageSpace, loading, noMessageSelected)
+	init
+	{
+		children.addAll(messageSpace, loading, noMessageSelected)
 
-        attachmentsMenu.graphic = Icons.attach()
+		attachmentsMenu.graphic = Icons.attach()
 
-        UIUtils.resizeable(viewer)
-        viewer.webView.engine.loadWorker.stateProperty().addListener { _, _, newState: Worker.State ->
-            if (newState == Worker.State.SUCCEEDED)
-            {
-                later { flip(messageSpace) ;                         /*loadAhead(messageProperty.get().second)*/
-                }
-            }
-        }
+		UIUtils.resizeable(viewer)
+		viewer.webView.engine.loadWorker.stateProperty().addListener { _, _, newState: Worker.State ->
+			if (newState == Worker.State.SUCCEEDED)
+			{
+				later {
+					flip(messageSpace)                         /*loadAhead(messageProperty.get().second)*/
+				}
+			}
+		}
 
-        val labelFont = Font.font(from.font.family, FontWeight.BOLD, from.font.size)
-        arrayOf(from, subject, to).forEach { it.font = labelFont }
+		val labelFont = Font.font(from.font.family, FontWeight.BOLD, from.font.size)
+		arrayOf(from, subject, to).forEach { it.font = labelFont }
 
-        headerLeft.addColumn(0, from, subject, to)
-        headerLeft.addColumn(1, fromText, subjectText, toText)
-        for (label in arrayOf(from, subject, to, fromText, subjectText, toText))
-        {
-            GridPane.setHgrow(label, Priority.NEVER)
-            GridPane.setVgrow(label, Priority.NEVER)
-            label.alignment = Pos.CENTER
-        }
+		headerLeft.addColumn(0, from, subject, to)
+		headerLeft.addColumn(1, fromText, subjectText, toText)
+		for (label in arrayOf(from, subject, to, fromText, subjectText, toText))
+		{
+			GridPane.setHgrow(label, Priority.NEVER)
+			GridPane.setVgrow(label, Priority.NEVER)
+			label.alignment = Pos.CENTER
+		}
 
-        headerLeft.hgap = 15.0
-        headerLeft.vgap = 5.0
+		headerLeft.hgap = 15.0
+		headerLeft.vgap = 5.0
 
-        buttons.spacing = 1.0
-        buttons.alignment = Pos.CENTER_RIGHT
+		buttons.spacing = 1.0
+		buttons.alignment = Pos.CENTER_RIGHT
 
-        headerRight.spacing = 5.0
-        headerRight.alignment = Pos.TOP_RIGHT
+		headerRight.spacing = 5.0
+		headerRight.alignment = Pos.TOP_RIGHT
 
-        HBox.setHgrow(headerLeft, Priority.NEVER)
-        HBox.setHgrow(headerRight, Priority.ALWAYS)
+		HBox.setHgrow(headerLeft, Priority.NEVER)
+		HBox.setHgrow(headerRight, Priority.ALWAYS)
 
-        header.background = Background(BackgroundFill(Color.WHITESMOKE, null, null))
-        header.padding = Insets(3.0, 3.0, 5.0, 3.0)
+		header.background = Background(BackgroundFill(Color.WHITESMOKE, null, null))
+		header.padding = Insets(3.0, 3.0, 5.0, 3.0)
 
-        VBox.setVgrow(header, Priority.NEVER)
-        VBox.setVgrow(viewer, Priority.ALWAYS)
-        UIUtils.resizeable(messageSpace)
+		VBox.setVgrow(header, Priority.NEVER)
+		VBox.setVgrow(viewer, Priority.ALWAYS)
+		UIUtils.resizeable(messageSpace)
 
-        viewer.webView.engine.documentProperty().addListener(listener)
+		viewer.webView.engine.documentProperty().addListener(listener)
 
-        trustContentMenu.graphic = Icons.trustContentProvider()
+		trustContentMenu.graphic = Icons.trustContentProvider()
 
-        messageProperty.addListener { _ -> newMessage() }
-    }
+		messageProperty.addListener { _ -> newMessage() }
+	}
 
-    fun setMessage(message: Pair<IMessage?, Collection<IMessage>>)
-    {
-        messageProperty.set(message)
-    }
+	fun setMessage(message: Pair<IMessage?, Collection<IMessage>>)
+	{
+		messageProperty.set(message)
+	}
 
-    private fun loadAhead(messages : Collection<IMessage> )
-    {
-        //val pos = model.selectedIndices[model.selectedIndices.size - 1]
-        //  TODO -- should do better than this, expand outwards, especially if we have a multi-selection
-        //  load ahead radially 4 messages either side of pos
-       // val n = model.tableView.items.size
-        println("Starting loadAhead")
-        messages.forEach { it.ensureContentLoaded(false)}
-//        for (i in 1 until 5)
-//        {
-//            val before = pos - i
-//            if (before in 0 until n)
-//            {
-//                model.tableView.items[before].ensureContentLoaded(false)
-//            }
-//            val after = pos + i
-//            if (after in 0 until n)
-//            {
-//                model.tableView.items[after].ensureContentLoaded(false)
-//            }
-//        }
-    }
+//    private fun loadAhead(messages : Collection<IMessage> )
+//    {
+//        //val pos = model.selectedIndices[model.selectedIndices.size - 1]
+//        //  TODO -- should do better than this, expand outwards, especially if we have a multi-selection
+//        //  load ahead radially 4 messages either side of pos
+//       // val n = model.tableView.items.size
+//        println("Starting loadAhead")
+//        messages.forEach { it.ensureContentLoaded(false)}
+////        for (i in 1 until 5)
+////        {
+////            val before = pos - i
+////            if (before in 0 until n)
+////            {
+////                model.tableView.items[before].ensureContentLoaded(false)
+////            }
+////            val after = pos + i
+////            if (after in 0 until n)
+////            {
+////                model.tableView.items[after].ensureContentLoaded(false)
+////            }
+////        }
+//    }
 
-    private fun newMessage()
-    {
-        val message = messageProperty.get().first
-        if (message != null)
-        {
-            println("FLIP Loading")
-            flip(loading)
-            println("STARTING TASK")
+	private fun newMessage()
+	{
+		val message = messageProperty.get().first
+		if (message != null)
+		{
+			println("FLIP Loading")
+			flip(loading)
+			println("STARTING TASK")
 
-            val task = object : Task<Void>()
-            {
-                override fun call(): Void?
-                {
-                    val account = message.mailAccount
-                    println("Client grabbing content :: " + message.id)
-                    val part = message.getContent(account.allowHTMLProperty.get())
+			val task = object : Task<Void>()
+			{
+				override fun call(): Void?
+				{
+					val account = message.mailAccount
+					println("Client grabbing content :: " + message.id)
+					val part = message.getContent(account.allowHTMLProperty.get())
 
-                    later {
-                        try
-                        {
-                            println("Client GOT content :: " + message.id)
-                            //  disable and clear trustContent menu until the load finishes
-                            trustContentMenu.items.clear()
-                            trustContentMenu.isDisable = true
+					later {
+						try
+						{
+                            //  they may have clicked on a new message in the time between the task was started
+                            //  and the time at which we get to this point
+							if (message == messageProperty.get().first)
+							{
+								println("Client GOT content :: " + message.id)
+								//  disable and clear trustContent menu until the load finishes
+								trustContentMenu.items.clear()
+								trustContentMenu.isDisable = true
 
-                            loadRemoteAction.disabledProperty().unbind()
-                            loadRemoteAction.isDisabled = true
+								loadRemoteAction.disabledProperty().unbind()
+								loadRemoteAction.isDisabled = true
 
-                            trustSenderAction.disabledProperty().unbind()
-                            trustSenderAction.disabledProperty().bind(Bindings.createBooleanBinding(
-                                    UIUtils.callable { account.isTrustedSender(message.from) }, account.trustedSenders))
+								trustSenderAction.disabledProperty().unbind()
+								trustSenderAction.disabledProperty().bind(Bindings.createBooleanBinding(
+										UIUtils.callable { account.isTrustedSender(message.from) }, account.trustedSenders))
 
-                            fromText.text = EmailAddress.format(message.mailAccount, message.from)
-                            subjectText.text = Format.formatN(message.subjectProperty.get())
-                            toText.text = EmailAddress.format(message.mailAccount, message.to)
-                            receivedOn.text = Format.asDate(message.receivedOnProperty.get())
+								fromText.text = EmailAddress.format(message.mailAccount, message.from)
+								subjectText.text = Format.formatN(message.subjectProperty.get())
+								toText.text = EmailAddress.format(message.mailAccount, message.to)
+								receivedOn.text = Format.asDate(message.receivedOnProperty.get())
 
-                            println("Client LOAD content :: " + message.id)
-                            viewer.loadContent(part.content, part.mimeType)
+								println("Client LOAD content :: " + message.id)
+								viewer.loadContent(part.content, part.mimeType)
 
-                            println("Client LOAD ATTACHMENTS :: " + message.id)
-                            //  call getAttachments() once because they do database queries so can be slow
-                            val attachments = message.attachments
-                            if (attachments.isEmpty())
-                            {
-                                attachmentsMenu.isDisable = true
-                            } else
-                            {
-                                attachmentsMenu.isDisable = false
-                                attachmentsMenu.items.clear()
-                                Attachments.viewSaveMenu(attachments, attachmentsMenu.items)
-                            }
+								println("Client LOAD ATTACHMENTS :: " + message.id)
+								//  call getAttachments() once because they do database queries so can be slow
+								val attachments = message.attachments
+								if (attachments.isEmpty())
+								{
+									attachmentsMenu.isDisable = true
+								} else
+								{
+									attachmentsMenu.isDisable = false
+									attachmentsMenu.items.clear()
+									Attachments.viewSaveMenu(attachments, attachmentsMenu.items)
+								}
 
-                            with(message) {
-                                loadRemoteProperty.addListener { _ -> viewer.reload() }
-                                account.trustedContentProviders.addListener { _: ListChangeListener.Change<out String> -> viewer.reload() }
-                                account.trustedSenders.addListener { _: ListChangeListener.Change<out EmailAddress> -> viewer.reload() }
-                                loadRemoteProperty.set(loadRemoteProperty.get() || account.isTrustedSender(from))
-                            }
-                        } catch (ex: Exception)
-                        {
-                            logger.warning(ex.localizedMessage)
-                            viewer.clear()
-                            Fail.fail(ex)
-                        }
+								with(message) {
+									loadRemoteProperty.addListener { _ -> viewer.reload() }
+									account.trustedContentProviders.addListener { _: ListChangeListener.Change<out String> -> viewer.reload() }
+									account.trustedSenders.addListener { _: ListChangeListener.Change<out EmailAddress> -> viewer.reload() }
+									loadRemoteProperty.set(loadRemoteProperty.get() || account.isTrustedSender(from))
+								}
+							}
+                            else
+                                println("IGNORING MESSAGE")
+						}
+						catch (exception: Exception)
+						{
+							logger.warning(exception.localizedMessage)
+							viewer.clear()
+							Fail.fail(exception)
+						}
+					}
 
-                    }
+					return null
+				}
+			}
 
-                    return null;
-                }
-            }
-
-            service.submit(task)
-        }
-    }
+			service.submit(task)
+		}
+	}
 }
